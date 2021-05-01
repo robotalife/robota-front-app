920d21bfd4936304491635173efe18bb80419a23
Ali Azimipour <ali.azimipour@jumbo.com>
2021-05-02 00:31:21 +0200

    adding piaf theme asset files

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-# front-app
+# piaf-vue
 
 ## Project setup
 ```
-yarn install
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+npm run serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
+npm run build
 ```
 
 ### Customize configuration
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-100">
+    <router-view />
+    <color-switcher />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorSwitcher from "./components/Common/ColorSwitcher";
+
+import { getDirection } from "./utils";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    "color-switcher": ColorSwitcher
+  },
+  beforeMount() {
+    const direction = getDirection();
+    if (direction.isRtl) {
+      document.body.classList.add("rtl");
+      document.dir = "rtl";
+      document.body.classList.remove("ltr");
+    } else {
+      document.body.classList.add("ltr");
+      document.dir = "ltr";
+      document.body.classList.remove("rtl");
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
