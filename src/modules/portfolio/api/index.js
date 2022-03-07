7b915f098d5ed206fcf28beba1794a48278a2f76
HERBOD7 <herbodabdoli@gmail.com>
2022-03-07 22:32:56 +0330

    feat: add symbols icon

--- src/modules/portfolio/api/index.js
+++ src/modules/portfolio/api/index.js
@@ -2,6 +2,7 @@ import $http from "@/services/http";
 
 const PIE_CHART_ENDPOINT = (id) => `/portfolio/pie?exchangeId=${id}`;
 const BALANCES_ENDPOINT = (id) => `/portfolio/balances?exchangeId=${id}`;
+const BALANCE_ENDPOINT = (id) => `/portfolio/balance?exchangeId=${id}`;
 
 export default {
   fetchPieChartData(id) {
@@ -9,5 +10,8 @@ export default {
   },
   fetchBalances(id) {
     return $http.get(BALANCES_ENDPOINT(id));
+  },
+  fetchBalance(id) {
+    return $http.get(BALANCE_ENDPOINT(id));
   },
 };
