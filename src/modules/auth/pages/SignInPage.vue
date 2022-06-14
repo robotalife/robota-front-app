c29e00b4e1c34840646fa59c60f0944326017b4f
HERBOD7 <herbodabdoli@gmail.com>
2022-06-14 21:09:13 +0430

    fix: add vuetify validation

--- src/modules/auth/pages/SignInPage.vue
+++ src/modules/auth/pages/SignInPage.vue
@@ -1,11 +1,11 @@
 <script>
-import BaseInput from "@/components/input/BaseInput.vue";
+// import BaseInput from "@/components/input/BaseInput.vue";
 import BaseButton from "@/components/button/BaseButton.vue";
 import storage from "@/utils/storage";
 export default {
   name: "SignIn",
   components: {
-    BaseInput,
+    // BaseInput,
     BaseButton,
   },
   data() {
@@ -17,53 +17,96 @@ export default {
       errorMessage: "",
       snackbarColor: "pink",
       snackbar: false,
-      isFormValid: false,
+      isFormValid: true,
       feildsValidation: {
         email: false,
         password: false,
       },
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      passwordRules: [(v) => !!v || "Password is required"],
       isButtonLoading: false,
     };
   },
   methods: {
     submit() {
-      this.isButtonLoading = true;
-      this.$api.auth
-        .loginUser(this.formData.email, this.formData.password)
-        .then((result) => {
-          this.isButtonLoading = false;
-          storage.setItem("token", result.token);
-          this.$store.commit("SET_USER", {
-            id: result.id,
-            email: this.emailAddress,
+      this.$refs.form.validate();
+      console.log(this.isFormValid, "check validation");
+      if (!this.isFormValid) {
+        this.isButtonLoading = true;
+        this.$api.auth
+          .loginUser(this.formData.email, this.formData.password)
+          .then((result) => {
+            this.isButtonLoading = false;
+            storage.setItem("token", result.token);
+            this.$store.commit("SET_USER", {
+              id: result.id,
+              email: this.emailAddress,
+            });
+            this.$router.push({ name: "dashboard" });
+          })
+          .catch((error) => {
+            this.isButtonLoading = false;
+            this.snackbar = true;
+            this.errorMessage = error?.response?.data.message;
           });
-          this.$router.push({ name: "dashboard" });
-        })
-        .catch((error) => {
-          this.isButtonLoading = false;
-          this.snackbar = true;
-          this.errorMessage = error?.response?.data.message;
-        });
+      }
+      return false;
     },
-    changeForm() {
-      // const value = e.target.value;
-      // const name = e.target.name;
-      // this.formData[name] = value;
-    },
-    validateInput(value) {
-      this.feildsValidation[value.feildName] = value.validtionStatus;
-      const feildsValidationStatus = Object.values(this.feildsValidation);
-      feildsValidationStatus.includes(false)
-        ? (this.isFormValid = false)
-        : (this.isFormValid = true);
-    },
+    // changeForm() {
+    // const value = e.target.value;
+    // const name = e.target.name;
+    // this.formData[name] = value;
+    // },
+    // validateInput(value) {
+    //   this.feildsValidation[value.feildName] = value.validtionStatus;
+    //   const feildsValidationStatus = Object.values(this.feildsValidation);
+    //   feildsValidationStatus.includes(false)
+    //     ? (this.isFormValid = false)
+    //     : (this.isFormValid = true);
+    // },
   },
 };
 </script>
 <template>
   <div>
     <p class="font-h-3 brand-purple fw-700">Sign In</p>
-    <form class="m-t-3" @submit.prevent="submit" @change="changeForm">
+    <v-form
+      ref="form"
+      v-model="isFormValid"
+      @submit.prevent="submit"
+      class="m-t-3"
+      lazy-validation
+    >
+      <span>E-mail</span>
+      <v-text-field
+        v-model="formData.email"
+        class="Login__input"
+        :rules="emailRules"
+        required
+      ></v-text-field>
+
+      <span>Password</span>
+      <v-text-field
+        v-model="formData.password"
+        class="m-t-2 Login__input"
+        :rules="passwordRules"
+        type="password"
+        required
+      ></v-text-field>
+
+      <!-- <div @click="submit"> -->
+      <BaseButton
+        class="w-1-1 m-t-3 Login__submit"
+        text="Sign Up"
+        size="small"
+        :isLoading="isButtonLoading"
+      />
+      <!-- </div> -->
+    </v-form>
+    <!-- <form class="m-t-3" @submit.prevent="submit" @change="changeForm">
       <BaseInput
         label="Email Address"
         type="email"
@@ -86,33 +129,33 @@ export default {
         text="Login"
         size="small"
         :isLoading="isButtonLoading"
-      />
-      <div class="d-flex m-t-4 jc-center fw-500">
-        <p class="font-14-24 g-2 m-r-0-5">Forgot your password?</p>
-        <RouterLink to="/forgot-password" class="Login__reset">
-          Reset Password
-        </RouterLink>
-      </div>
-      <div class="d-flex m-t-2 jc-center fw-500">
-        <p class="font-14-24 g-2 m-r-0-5">
-          Didn’t create an account ?
-          <RouterLink to="/signup" class="Login__reset"> Sign Up </RouterLink>
-        </p>
-      </div>
-      <v-snackbar v-model="snackbar" :right="true" :multi-line="true">
-        {{ errorMessage }}
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            :color="snackbarColor"
-            text
-            v-bind="attrs"
-            @click="snackbar = false"
-          >
-            close
-          </v-btn>
-        </template>
-      </v-snackbar>
-    </form>
+      /> -->
+    <div class="d-flex m-t-4 jc-center fw-500">
+      <p class="font-14-24 g-2 m-r-0-5">Forgot your password?</p>
+      <RouterLink to="/forgot-password" class="Login__reset">
+        Reset Password
+      </RouterLink>
+    </div>
+    <div class="d-flex m-t-2 jc-center fw-500">
+      <p class="font-14-24 g-2 m-r-0-5">
+        Didn’t create an account ?
+        <RouterLink to="/signup" class="Login__reset"> Sign Up </RouterLink>
+      </p>
+    </div>
+    <v-snackbar v-model="snackbar" :right="true" :multi-line="true">
+      {{ errorMessage }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          :color="snackbarColor"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <!-- </form> -->
   </div>
 </template>
 
@@ -130,6 +173,17 @@ export default {
       font-size: 13px;
       line-height: 24px;
       font-weight: 700;
+    }
+  }
+
+  @include e(input) {
+    padding: 0px !important;
+    ::v-deep {
+      .v-input__slot {
+        height: 48px;
+        border-radius: 5px;
+        border: solid 1px $dark-blue-20;
+      }
     }
   }
 
--- src/modules/auth/pages/SignupPage.vue
+++ src/modules/auth/pages/SignupPage.vue
@@ -1,10 +1,10 @@
 <script>
-import BaseInput from "@/components/input/BaseInput.vue";
+// import BaseInput from "@/components/input/BaseInput.vue";
 import BaseButton from "@/components/button/BaseButton.vue";
 export default {
   name: "SignUp",
   components: {
-    BaseInput,
+    // BaseInput,
     BaseButton,
   },
   data() {
@@ -13,7 +13,7 @@ export default {
       snackbarColor: "pink",
       snackbar: false,
       termsAndConditions: false,
-      isFormValid: false,
+      isFormValid: true,
       feildsValidation: {
         email: false,
         password: false,
@@ -23,49 +23,110 @@ export default {
       formData: {
         email: "",
         password: "",
-        "rp-password": "",
+        confirmPassword: "",
+        terms: false,
       },
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      passwordRules: [(v) => !!v || "Password is required"],
+      confirmPasswordRules: [
+        [(v) => !!v || "Password is required"],
+        this.password === this.confirmPassword || "Password must match",
+      ],
       isButtonLoading: false,
     };
   },
   methods: {
     submit() {
-      this.isButtonLoading = true;
-      this.$api.auth
-        .registerUser(this.formData.email, this.formData.password)
-        .then(() => {
-          this.isButtonLoading = false;
-          this.snackbar = true;
-          this.snackbarColor = "green";
-          this.errorMessage =
-            "Please check your email for the activation email.";
-          setTimeout(() => this.$router.push({ name: "signIn" }), 15000);
-        })
-        .catch((error) => {
-          this.isButtonLoading = false;
-          this.snackbar = true;
-          this.errorMessage = error?.response?.data.message;
-        });
+      this.$refs.form.validate();
+      if (!this.isFormValid) {
+        this.isButtonLoading = true;
+        this.$api.auth
+          .registerUser(this.formData.email, this.formData.password)
+          .then(() => {
+            this.isButtonLoading = false;
+            this.snackbar = true;
+            this.snackbarColor = "green";
+            this.errorMessage =
+              "Please check your email for the activation email.";
+            setTimeout(() => this.$router.push({ name: "signIn" }), 15000);
+          })
+          .catch((error) => {
+            this.isButtonLoading = false;
+            this.snackbar = true;
+            this.errorMessage = error?.response?.data.message;
+          });
+      }
     },
-    changeForm() {
-      // const value = e.target.value;
-      // const name = e.target.name;
-      // this.formData[name] = value;
-    },
-    validateInput(value) {
-      this.feildsValidation[value.feildName] = value.validtionStatus;
-      const feildsValidationStatus = Object.values(this.feildsValidation);
-      feildsValidationStatus.includes(false)
-        ? (this.isFormValid = false)
-        : (this.isFormValid = true);
-    },
+    // changeForm() {
+    // const value = e.target.value;
+    // const name = e.target.name;
+    // this.formData[name] = value;
+    // },
+    // validateInput(value) {
+    //   this.feildsValidation[value.feildName] = value.validtionStatus;
+    //   const feildsValidationStatus = Object.values(this.feildsValidation);
+    //   feildsValidationStatus.includes(false)
+    //     ? (this.isFormValid = false)
+    //     : (this.isFormValid = true);
+    // },
   },
 };
 </script>
 <template>
   <div>
     <p class="font-h-3 brand-purple fw-700">Sign Up</p>
-    <form class="m-t-3" @submit.prevent="submit" @change="changeForm">
+    <v-form
+      ref="form"
+      v-model="isFormValid"
+      @submit.prevent="submit"
+      class="m-t-3"
+      lazy-validation
+    >
+      <v-text-field
+        v-model="formData.email"
+        class="SignUp__input"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.password"
+        class="m-t-2 SignUp__input"
+        :rules="passwordRules"
+        label="Password"
+        type="password"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.confirmPassword"
+        class="m-t-2 SignUp__input"
+        :rules="confirmPasswordRules"
+        label="Repeat Password"
+        type="password"
+        required
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="formData.terms"
+        class="m-t-2"
+        :rules="[(v) => !!v || 'You must agree to continue!']"
+        label="Do you agree?"
+        required
+      ></v-checkbox>
+
+      <BaseButton
+        class="w-1-1 m-t-3 SignUp__submit"
+        text="Sign Up"
+        size="small"
+        :isLoading="isButtonLoading"
+      />
+    </v-form>
+    <!-- <form class="m-t-3" @submit.prevent="submit" @change="changeForm">
       <BaseInput
         label="Email Address"
         type="email"
@@ -120,27 +181,27 @@ export default {
         text="Sign Up"
         size="small"
         :isLoading="isButtonLoading"
-      />
-      <div class="d-flex m-t-5 jc-center fw-500">
-        <p class="font-14-24 g-65 m-r-0-5">
-          Already have an account?
-          <RouterLink to="/login" class="SignUp__reset"> Sign In </RouterLink>
-        </p>
-      </div>
-      <v-snackbar v-model="snackbar" :right="true" :multi-line="true">
-        {{ errorMessage }}
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            :color="snackbarColor"
-            text
-            v-bind="attrs"
-            @click="snackbar = false"
-          >
-            close
-          </v-btn>
-        </template>
-      </v-snackbar>
-    </form>
+      /> -->
+    <div class="d-flex m-t-5 jc-center fw-500">
+      <p class="font-14-24 g-65 m-r-0-5">
+        Already have an account?
+        <RouterLink to="/login" class="SignUp__reset"> Sign In </RouterLink>
+      </p>
+    </div>
+    <v-snackbar v-model="snackbar" :right="true" :multi-line="true">
+      {{ errorMessage }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          :color="snackbarColor"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <!-- </form> -->
   </div>
 </template>
 
@@ -158,6 +219,17 @@ export default {
       font-size: 13px;
       line-height: 24px;
       font-weight: 700;
+    }
+  }
+
+  @include e(input) {
+    padding: 0px !important;
+    ::v-deep {
+      .v-input__slot {
+        height: 48px;
+        border-radius: 5px;
+        border: solid 1px $dark-blue-20;
+      }
     }
   }
 
