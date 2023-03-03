e8c604390388648b39290faed04547a47a39327f
homam <h.ghassemy@oxolo.com>
2023-03-03 20:42:53 +0330

    added scss supprot for css modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: [
-  //     { find: "@assets", replacement: "/src/assets" },
-  //     { find: "@components", replacement: "/src/components" },
-  //     { find: "@pages", replacement: "/src/pages" },
-  //   ],
-  // },
+  resolve: {
+    alias: [
+      { find: "@styles", replacement: "./src/assets/styles" },
+      { find: "@components", replacement: "./src/components" },
+    ],
+  },
+  css: {
+    postcss: null,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/styles/variables.scss";`,
+      },
+    },
+  },
 });
