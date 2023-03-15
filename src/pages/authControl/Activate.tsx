9e1e81408ef41374ffd9379275d4ea048274e95f
homam <h.ghassemy@oxolo.com>
2023-03-16 01:15:10 +0330

    signun, activate signin done

--- src/pages/authControl/Activate.tsx
+++ src/pages/authControl/Activate.tsx
@@ -1,28 +1,32 @@
 import { Grid, Typography } from "@mui/material";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/formElements/Button";
-import Checkbox from "../../components/formElements/Checkbox";
 import TextField from "../../components/formElements/TextField";
 import routes from "../../shared/consts/routes";
 
 import circleCheck from "@images/green-circle-check.svg";
-import { useEffect } from "react";
 import useAxios from "../../shared/hooks/useAxios";
 import apiEndPoints from "../../shared/consts/apiEndpoints";
 import { AxiosResponse } from "axios";
+import * as Yup from "yup";
+import { Form, Formik } from "formik";
+
+const validationSchema = Yup.object().shape({
+  key: Yup.string().required("Required"),
+});
 
 const Activate = () => {
-  const { id } = useParams();
-  const { axios, Snackbar } = useAxios();
+  const navigate = useNavigate();
+  const { axios } = useAxios();
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (values: { key: string }) => {
     try {
       const response: AxiosResponse<any, any> = await axios.post(
-        `${apiEndPoints.activate}${id}`
+        `${apiEndPoints.activate}${values.key}`
       );
 
+      navigate(routes.signin);
       console.log(response);
-
       // Handle successful response
     } catch (error) {
       // Handle error
@@ -30,42 +34,65 @@ const Activate = () => {
   };
 
   return (
-    <div>
-      <div style={{ textAlign: "center" }}>
-        <img src={circleCheck} />
-      </div>
-      <div className={"pageTitle"}>Check your email</div>
-      <div className={"pageSubtitle"} style={{ marginBottom: 50 }}>
-        If you do not receive a confirmation email, please check your spam
-        folder. Also, please verify that you entered a valid email address in
-        our sign-up form.
-      </div>
-      <TextField />
-      <Typography sx={{ mb: 1, mt: 1 }}>
-        <Link to={routes.resetPassword}>Forget Password?</Link>
-      </Typography>
+    <Formik
+      initialValues={{ key: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        handleSubmit(values);
+        setSubmitting(false);
+      }}
+    >
+      {({ values, handleChange, errors, touched }) => (
+        <Form noValidate>
+          <div style={{ textAlign: "center" }}>
+            <img src={circleCheck} />
+          </div>
+          <div className={"pageTitle"}>Check your email</div>
+          <div className={"pageSubtitle"} style={{ marginBottom: 50 }}>
+            If you do not receive a confirmation email, please check your spam
+            folder. Also, please verify that you entered a valid email address
+            in our sign-up form.
+          </div>
+          <TextField
+            name="key"
+            type="text"
+            value={values.key}
+            onChange={handleChange}
+            required
+            error={Boolean(errors.key && touched.key)}
+            helperText={errors.key && touched.key && errors.key}
+          />
+          <Typography sx={{ mb: 1, mt: 1 }}>
+            <Link to={routes.resetPassword}>Forget Password?</Link>
+          </Typography>
 
-      <Button variant="contained" sx={{ my: 4, mx: "auto", display: "block" }}>
-        Verify email
-      </Button>
-      <Typography
-        component={"div"}
-        sx={{ textAlign: "center" }}
-        alignItems="center"
-      >
-        <Grid container spacing={2}>
-          <Grid item>Didn’t receive the email? </Grid>
-          <Grid item xs={"auto"}>
-            <Button variant="text" sx={{ m: 0, p: 0, display: "none" }}>
-              Click to resend
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <Link to={routes.signup}>Back to Sign Up</Link>
-          </Grid>
-        </Grid>
-      </Typography>
-    </div>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ my: 4, mx: "auto", display: "block" }}
+          >
+            Verify email
+          </Button>
+          <Typography
+            component={"div"}
+            sx={{ textAlign: "center" }}
+            alignItems="center"
+          >
+            <Grid container spacing={2}>
+              <Grid item>Didn’t receive the email? </Grid>
+              <Grid item xs={"auto"}>
+                <Button variant="text" sx={{ m: 0, p: 0 }}>
+                  Click to resend
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Link to={routes.signup}>Back to Sign Up</Link>
+              </Grid>
+            </Grid>
+          </Typography>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
