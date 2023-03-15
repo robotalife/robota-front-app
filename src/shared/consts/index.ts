53c5bf59bceae348b4093e89a476db74e0c2ff37
homam <h.ghassemy@oxolo.com>
2023-03-15 22:47:12 +0330

    base url reading from the env file now

--- src/shared/consts/index.ts
+++ src/shared/consts/index.ts
@@ -1,3 +1,13 @@
 const process = import.meta.env;
 
-export const API_BASE_URL = process.VITE_DEVELOPMENT_BASE_URL;
+type EnvironmentType = "development" | "staging" | "production";
+
+const environment = process.VITE_NODE_ENV as EnvironmentType;
+
+const envs = {
+  development: "VITE_DEVELOPMENT_BASE_URL",
+  staging: "VITE_STAGING_BASE_URL",
+  production: "VITE_PODUCTION_BASE_URL",
+} as { [k in EnvironmentType]: string };
+
+export const API_BASE_URL = process[envs[environment]];
