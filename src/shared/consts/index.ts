98e4aeea2e402200820724a20d1dbb0057d5460b
homam <h.ghassemy@oxolo.com>
2023-04-05 07:39:00 +0330

    env typo fixed

--- src/shared/consts/index.ts
+++ src/shared/consts/index.ts
@@ -7,7 +7,7 @@ const environment = process.VITE_NODE_ENV as EnvironmentType;
 const envs = {
   development: "VITE_DEVELOPMENT_BASE_URL",
   staging: "VITE_STAGING_BASE_URL",
-  production: "VITE_PODUCTION_BASE_URL",
+  production: "VITE_PRODUCTION_BASE_URL",
 } as { [k in EnvironmentType]: string };
 
 export const API_BASE_URL = process[envs[environment]];
