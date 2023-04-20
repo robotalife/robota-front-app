f13355e71629a38769fc4a605d55a6da2227be51
homam <h.ghassemy@oxolo.com>
2023-04-20 17:41:30 +0330

    added disabled pagination to history

---
+++ src/components/shared/Pagination.tsx
@@ -0,0 +1,22 @@
+import { Grid, Typography } from "@mui/material";
+import Button from "../formElements/Button";
+
+const Pagination = () => {
+  return (
+    <Grid container justifyContent={"space-between"} alignItems={"center"}>
+      <Grid item xs={"auto"}>
+        <Typography component={"div"}>Page 1 of 10</Typography>
+      </Grid>
+      <Grid item xs={"auto"}>
+        <Button variant="outlined" sx={{ mr: 1 }} disabled>
+          Previous
+        </Button>
+        <Button variant="outlined" disabled>
+          Next
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Pagination;
