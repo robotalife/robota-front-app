b844a0a7de56a9c92cdcbd14fe51c21670efc2e1
homam <h.ghassemy@oxolo.com>
2023-04-21 10:04:39 +0330

    month names added to date strings

--- src/shared/helpers/getDateTimeObj.ts
+++ src/shared/helpers/getDateTimeObj.ts
@@ -2,8 +2,28 @@ const getDateTime = (date: string): { time: string; date: string } => {
   const tmpObj = { time: "", date: "" };
   const dateObj = new Date(date);
 
+  // Define an array of month names
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
   tmpObj.time = `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
-  tmpObj.date = `${dateObj.getDay()} ${dateObj.getMonth()} ${dateObj.getFullYear()}`;
+
+  // Use the month index to get the short month name from the array
+  tmpObj.date = `${dateObj.getDay()} ${
+    months[dateObj.getMonth()]
+  } ${dateObj.getFullYear()}`;
 
   return tmpObj;
 };
