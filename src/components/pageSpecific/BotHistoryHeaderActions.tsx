d2b5bb837af98cf403f03ae813cb4762a9c27f28
homam <h.ghassemy@oxolo.com>
2023-04-22 19:17:47 +0330

    filter button action to show/hide filters

---
+++ src/components/pageSpecific/BotHistoryHeaderActions.tsx
@@ -0,0 +1,13 @@
+const BotHistoryHeaderActions = () => {
+  //   const downloadLink = (src: string) => {
+  //     const link = document.createElement("a");
+  //     link.href = src;
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   };
+
+  return <div>BotHistoryHeaderActions</div>;
+};
+
+export default BotHistoryHeaderActions;
---
+++ src/shared/interfaces/exchange.ts
@@ -0,0 +1,37 @@
+export interface IExchangeListResponseObj {
+  default: boolean;
+  exchangeId: string;
+  exchangeName: string;
+  exchangeType: string;
+}
+export interface IExchange extends IExchangeListResponseObj {
+  label: string;
+  value: string;
+}
+
+export interface IPair {
+  value: string;
+  text: string;
+  base: string;
+  quote: string;
+  symbolFilter: {
+    lotSizeFilter: {
+      minQty: string;
+      maxQty: string;
+      stepSize: string;
+      maxAllowedPrecision: number;
+    };
+    priceFilter: {
+      minPrice: string;
+      maxPrice: string;
+      tickSize: string;
+    };
+  };
+}
+
+export interface IExchangeContext {
+  exchangeList: IExchange[];
+  selectedExchange: string | undefined;
+  setSelectedExchange: (exchange: string) => void;
+  pairs: IPair[];
+}
