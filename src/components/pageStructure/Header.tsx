e82df6881cba17243058d46eee5c80a0fa101324
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2023-06-24 17:37:48 +0200

    feat: adding beta label to logo

--- src/components/pageStructure/Header.tsx
+++ src/components/pageStructure/Header.tsx
@@ -1,26 +1,42 @@
 import {Grid} from "@mui/material";
+
 import robotaLogoType from "@images/robota-logo-type.svg";
 import classes from "./Header.module.scss";
-import {Container} from "@mui/system";
 import {NavLink} from "react-router-dom";
 import routes from "../../shared/consts/routes";
+import TextBadge from "../shared/TextBadge";
 
 const Header = () => {
     return (
-        <Container maxWidth={false}>
-            <Grid container justifyContent={"space-between"}>
-                <Grid item xs>
-                    <NavLink to={routes.botsNew} className={classes.logoLink}>
-                        <img src={robotaLogoType} className={classes.logo}/>
-                    </NavLink>
-                </Grid>
-                {/* <Grid item xs={"auto"}>
-          <IconButton>
-            <IconBell />
-          </IconButton>
-        </Grid> */}
+        // <Container maxWidth={false}>
+        //     <Grid container justifyContent={"space-between"} >
+        //         <Grid>
+        //             <Grid item>
+        //             <NavLink to={routes.botsNew} className={classes.logoLink}>
+        //                 <img src={robotaLogoType} className={classes.logo}/>
+        //             </NavLink>
+        //             </Grid>
+        //             <Grid>
+        //                 <TextBadge variation={"primary"} >Beta Version</TextBadge>
+        //             </Grid>
+        //         </Grid>
+        //         {/* <Grid item xs={"auto"}>
+        //   <IconButton>
+        //     <IconBell />
+        //   </IconButton>
+        // </Grid> */}
+        //     </Grid>
+        // </Container>
+        <Grid container alignItems={"center"} spacing={1} >
+            <Grid item xs={"auto"}>
+                <NavLink to={routes.botsNew} className={classes.logoLink}>
+                    <img src={robotaLogoType} className={classes.logo}/>
+                </NavLink>
             </Grid>
-        </Container>
+            <Grid item xs={"auto"}>
+                <TextBadge variation={"primary"} >Beta Version</TextBadge>
+            </Grid>
+        </Grid>
     );
 };
 
