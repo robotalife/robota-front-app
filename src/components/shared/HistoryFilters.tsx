8e4c1824a99d10db5934c67866e4f80667853b1d
Mahdi Ghasemi <mahdighasemi@EUIL-MAC084.local>
2023-07-16 18:04:12 +0200

    added range picker to bot history page

--- src/components/shared/HistoryFilters.tsx
+++ src/components/shared/HistoryFilters.tsx
@@ -1,7 +1,8 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import DatePicker from "../formElements/DatePicker";
 import moment from "moment";
 import { useState } from "react";
+import RangePicker from "../formElements/RangePicker";
 
 interface Props {
   onCreatedChange: (e: Date | null) => void;
@@ -12,8 +13,19 @@ const HistoryFilters = ({ onCreatedChange, onClosedChange }: Props) => {
   const [minClosedDate, setMinClosedDate] = useState<Date | null>(null);
   const minCreatedDate = moment(new Date()).subtract(90, "days");
 
+  const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12} lg={3}>
+        <Typography variant="h6">Created On</Typography>
+        <RangePicker
+          value={dateRange}
+          setValue={setDateRange}
+          disabled={false}
+          onChange={(e) => console.log(e)}
+        />
+      </Grid>
       <Grid item xs={12} lg={3}>
         <DatePicker
           label="Created On"
