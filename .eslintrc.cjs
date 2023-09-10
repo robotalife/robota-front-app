f1b9e78e5988c5a17ef8edb8ddbd04eb7a7e463a
Mahdi Ghasemi <mahdighasemi@EUIL-MAC084.home>
2023-09-10 11:34:32 +0200

    eslint settings updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "standard",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    // Customize rules as per your project's needs
-  },
-  settings: {
-    react: {
-      version: "detect", // Automatically includes the React version
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'standard',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            tsx: true,
+        },
     },
-  },
+    plugins: ['react', 'react-hooks', '@typescript-eslint'],
+    rules: {
+        // Customize rules as per your project's needs
+    },
+    settings: {
+        react: {
+            version: 'detect', // Automatically includes the React version
+        },
+    },
 };
